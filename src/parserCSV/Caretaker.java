e2e4963d07f8bfbfadfd1e5faa2886a2f05c4fbb
ctaxx <bykov.stanislav.p@gmail.com>
2017-11-30 19:01:06 +0200

    some bugs have been fixed

--- src/parserCSV/Caretaker.java
+++ src/parserCSV/Caretaker.java
@@ -3,21 +3,15 @@ package parserCSV;
 import java.util.*;
 
 public class Caretaker {
-    private List<Memento> stateList = new LinkedList<>();
+    private LinkedList<Memento> stateList = new LinkedList<>();
 
     public Memento getMemento(){
         if (stateList.isEmpty()){
             System.out.println("stateList is empty!");
             return null;
         }
-        Memento memento = stateList.get(stateList.size()-1);
-        stateList.remove(stateList.size()-1);
-//        System.out.println("I'm gonna return a memento!");
-//        System.out.println("this memento is:");
-//        for(Memento mem: stateList) {
-//            ParserCSV.printArray(mem.getState());
-//        }
-//        System.out.println("--------------");
+        Memento memento = stateList.getLast();
+        stateList.removeLast();
         return memento;
     }
 
